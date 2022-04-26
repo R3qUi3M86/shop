bd37e9783c4097d8e41b10bd6809cf94ccf4330d
Chomentowski Sebastian <chomentowski.s@gmail.com>
2022-04-26 21:04:47 +0200

    second version

--- src/main/java/com/codecool/codecoolshopspring/controller/ProductController.java
+++ src/main/java/com/codecool/codecoolshopspring/controller/ProductController.java
@@ -1,19 +1,14 @@
 package com.codecool.codecoolshopspring.controller;
 
-import com.codecool.codecoolshopspring.model.ProductCategory;
 import com.codecool.codecoolshopspring.service.ProductService;
-import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -36,19 +31,27 @@ public class ProductController {
     }
 
     @PostMapping("/")
-    public String filterByCategory(RedirectAttributes redirectAttributes, HttpSession session, HttpServletRequest request) {
+    public String filterByCategory(RedirectAttributes redirectAttributes, HttpServletRequest request) {
         String categoryId = request.getParameter("category-select");
         redirectAttributes.addAttribute("categoryId", Integer.valueOf(categoryId));
-        return "redirect:/display-by-category/{categoryId}";
+        return "redirect:/{categoryId}";
     }
 
-
-    @GetMapping("/display-by-category/{categoryId}")
+    @GetMapping("/{categoryId}")
     public String displayProductsByCategory(Model model, @PathVariable int categoryId) {
         model.addAttribute("category", service.getProductCategory(categoryId));
         model.addAttribute("products", service.getProductsForCategory(categoryId));
         model.addAttribute("allCategories", service.getAllProductCategories());
         return "product/index";
     }
+
+
+//    @GetMapping("/display-by-category/{categoryId}")
+//    public String displayProductsByCategory(Model model, @PathVariable int categoryId) {
+//        model.addAttribute("category", service.getProductCategory(categoryId));
+//        model.addAttribute("products", service.getProductsForCategory(categoryId));
+//        model.addAttribute("allCategories", service.getAllProductCategories());
+//        return "product/index";
+//    }
 
 }
