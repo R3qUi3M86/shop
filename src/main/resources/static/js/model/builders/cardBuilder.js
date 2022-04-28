c3f11bb37e2ad9eba54c641ff430fba0cacd36f3
Chomentowski Sebastian <chomentowski.s@gmail.com>
2022-04-28 20:10:13 +0200

    extracted function from cardBuilder

--- src/main/resources/static/js/model/builders/cardBuilder.js
+++ src/main/resources/static/js/model/builders/cardBuilder.js
@@ -6,57 +6,61 @@ export const cardTypes = {
     productCard: "product card"
 }
 
-export function cardBuilder(type, content){
+export function cardBuilder(type, product){
     switch (type){
         case cardTypes.productCard:
-            const productCardContainer = document.createElement("div");
-            productCardContainer.classList.add("col","col-sm-12", "col-md-6", "col-lg-4");
-
-            const productCard = document.createElement("div");
-            productCard.classList.add("card");
-
-            const productImg = document.createElement("img");
-            productImg.setAttribute("class", "");
-            productImg.src = 'img/product_' + content.id + '.jpg';
-            productImg.setAttribute("alt", "")
-
-            const cardHeader = document.createElement("div");
-            cardHeader.classList.add("card-header");
-
-            const productTitle = document.createElement("h4");
-            productTitle.classList.add("card-title");
-            productTitle.innerText = content.name;
-
-            const productDesc = document.createElement("p");
-            productDesc.classList.add("card-text");
-            productDesc.innerText = content.description;
-
-            const cardBody = document.createElement("div");
-            cardBody.classList.add("card-body");
-
-            const cardTextPrice = document.createElement("div");
-            cardTextPrice.classList.add("card-text");
-
-            const productPrice = document.createElement("p");
-            productPrice.classList.add("lead");
-            productPrice.innerText = content.defaultPrice + " " + content.defaultCurrency;
-
-            const cardTextBtn = document.createElement("div");
-            cardTextBtn.classList.add("card-text");
-
-            const addToCart = buttonBuilder(buttonTypes.productBtn, content.id);
-
-            productCardContainer.appendChild(productCard);
-            productCard.appendChild(productImg);
-            productCard.appendChild(cardHeader)
-            productCard.appendChild(cardBody)
-            cardHeader.appendChild(productTitle);
-            cardHeader.appendChild(productDesc);
-            cardBody.appendChild(cardTextPrice);
-            cardBody.appendChild(cardTextBtn);
-            cardTextPrice.appendChild(productPrice);
-            cardTextBtn.appendChild(addToCart);
-
-            return productCardContainer;
+            return buildProductCardContainer(product);
     }
+
+function buildProductCardContainer(product) {
+    const productCardContainer = document.createElement("div");
+    productCardContainer.classList.add("col", "col-sm-12", "col-md-6", "col-lg-4");
+
+    const productCard = document.createElement("div");
+    productCard.classList.add("card");
+
+    const productImg = document.createElement("img");
+    productImg.setAttribute("class", "");
+    productImg.src = 'img/product_' + product.id + '.jpg';
+    productImg.setAttribute("alt", "")
+
+    const cardHeader = document.createElement("div");
+    cardHeader.classList.add("card-header");
+
+    const productTitle = document.createElement("h4");
+    productTitle.classList.add("card-title");
+    productTitle.innerText = product.name;
+
+    const productDesc = document.createElement("p");
+    productDesc.classList.add("card-text");
+    productDesc.innerText = product.description;
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const cardTextPrice = document.createElement("div");
+    cardTextPrice.classList.add("card-text");
+
+    const productPrice = document.createElement("p");
+    productPrice.classList.add("lead");
+    productPrice.innerText = product.defaultPrice + " " + product.defaultCurrency;
+
+    const cardTextBtn = document.createElement("div");
+    cardTextBtn.classList.add("card-text");
+
+    const addToCart = buttonBuilder(buttonTypes.productBtn, product.id);
+
+    productCardContainer.appendChild(productCard);
+    productCard.appendChild(productImg);
+    productCard.appendChild(cardHeader)
+    productCard.appendChild(cardBody)
+    cardHeader.appendChild(productTitle);
+    cardHeader.appendChild(productDesc);
+    cardBody.appendChild(cardTextPrice);
+    cardBody.appendChild(cardTextBtn);
+    cardTextPrice.appendChild(productPrice);
+    cardTextBtn.appendChild(addToCart);
+
+    return productCardContainer;
+}
 }
