60220ced321bffdec1e29a03de704bd5ed189a96
Chomentowski Sebastian <chomentowski.s@gmail.com>
2022-05-12 17:50:31 +0200

    BillingDetails form validation

--- src/main/java/com/codecool/codecoolshopspring/model/billingDetails/BillingDetails.java
+++ src/main/java/com/codecool/codecoolshopspring/model/billingDetails/BillingDetails.java
@@ -1,7 +1,6 @@
 package com.codecool.codecoolshopspring.model.billingDetails;
 
 
-import com.sun.istack.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Data
@@ -19,12 +19,40 @@ public class BillingDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotEmpty
     private String firstName;
+    @NotEmpty
     private String lastName;
+    @NotEmpty
+    @Email
     private String email;
+    @NotEmpty
     private String address;
     private String address2;
+    @NotEmpty
     private String country;
+    @NotEmpty
     private String state;
+    @NotEmpty
+    @Size(min = 6, max = 6)
     private String zipCode;
+
+    public void removeRejectedValue(Object rejectedValue) {
+        System.out.println(rejectedValue);
+        if (firstName == rejectedValue) {
+            firstName = null;
+        } else if (lastName == rejectedValue) {
+            lastName = null;
+        } else if (email == rejectedValue) {
+            email = null;
+        } else if (address == rejectedValue) {
+            address = null;
+        } else if (country == rejectedValue) {
+            country = null;
+        } else if (state == rejectedValue) {
+            state = null;
+        } else if (zipCode == rejectedValue) {
+            zipCode = null;
+        }
+    }
 }
